a1ad0b7e1cb9257b5e5e47e13af2c9e27a054b76
sscps local <wrobins101@student.sscps.org>
2018-03-06 14:18:17 -0500

    Initial

---
+++ External.js
@@ -0,0 +1,38 @@
+
+function getIPAddress(callback, errorCallback) {
+  var IPUrl = 'temporary url'; // Any site used here must be present in manifest.json.
+  var thing = new XMLHttpRequest();
+  thing.open('GET', IPUrl);
+  thing.responseType = 'text';
+  thing.onload = function() {
+    var response = thing.response;
+    if (!response) {
+      errorCallback('No response.');
+      return;
+    }
+
+	var pattern = /\b(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])\b/;
+	var foundIP = pattern.exec(response);
+
+    callback(foundIP[0]);
+  };
+  thing.onerror = function() {
+    errorCallback('Network error.');
+  };
+  thing.send();
+}
+
+function renderText(statusText) {
+	document.getElementById('status').textContent = statusText;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    getIPAddress(function(ipaddress) {
+
+      renderText(ipaddress);
+
+    }, function(errorMessage) {
+    });
+});
