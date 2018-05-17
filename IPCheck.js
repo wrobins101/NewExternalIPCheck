bb60c62f694cddd5a147263313d17095bd56422e
wrobins101 <32552572+wrobins101@users.noreply.github.com>
2018-05-17 11:28:24 -0700

    Added street

--- IPCheck.js
+++ IPCheck.js
@@ -37,7 +37,6 @@ function getLocalIPs(callback) {
 function getIPAddress(callback, errorCallback) {
   var IPUrl = 'https://showextip.azurewebsites.net/'; // Any site used here must be present in manifest.json.
   var thing = new XMLHttpRequest();
-  //var test = '50.235.191.182'
   thing.open('GET', IPUrl);
   thing.responseType = 'text';
   thing.onload = function() {
@@ -56,14 +55,13 @@ function getIPAddress(callback, errorCallback) {
     errorCallback('Network error.');
   };
   thing.send();
+}
 
 function renderText(statusText) {
+  statusText = statusText + String.fromCharCode(13) + ' (700 longwater)'
 	document.getElementById('status').textContent = statusText;
 }
 
-if( statusText != '50.235.191.183') {
-return;
-}
 
 document.addEventListener('DOMContentLoaded', function() {
 
